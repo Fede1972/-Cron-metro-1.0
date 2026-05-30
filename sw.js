fd5710f79fab8a0c3240ed74248320c33d4cc5a4
Fede1972 <federicombravo@gmail.com>
2026-05-30 17:20:08 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sbdo-v10';
+const CACHE = 'sbdo-v11';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
